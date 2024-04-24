7eaefbc099c8f9f0cbb94dc7328145871ac5cf7e
soum3n0 <soumen0dey22@gmail.com>
2024-04-24 11:53:34 +0530

    tictactoe modified

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -13,12 +13,12 @@ let gameGrid;
 let player;
 let comp;
 let winnerList = [
-    [0,1,2], [3,4,5], [6,7,8],
-    [0,3,6], [1,4,7], [2,5,8],
-    [0,4,8], [2,4,6]
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
 ];
 
-function initGrid(){
+function initGrid() {
     homeScreen.classList.add("active");
     comp = false;
     player = 'X';
@@ -26,7 +26,7 @@ function initGrid(){
     gameInfo.innerText = `Current Player - ${player}`;
 
     //Clear grid
-    boxes.forEach((box) =>{
+    boxes.forEach((box) => {
         box.innerText = "";
         box.style.pointerEvents = "all";
         // Give default CSS property
@@ -38,49 +38,49 @@ function initGrid(){
 
 initGrid();
 
-function disableCurser(){
-    boxes.forEach((box)=>{
+function disableCurser() {
+    boxes.forEach((box) => {
         box.style.pointerEvents = 'none';
     });
 }
 
-function enableCurser(){
-    boxes.forEach((box)=>{
+function enableCurser() {
+    boxes.forEach((box) => {
         box.style.pointerEvents = 'all';
     });
 }
 
-function checkGameOver(){
+function checkGameOver() {
     let winner = "";
-    winnerList.forEach((arr)=>{
-        if(gameGrid[arr[0]] === gameGrid[arr[1]] && gameGrid[arr[1]] === gameGrid[arr[2]] && gameGrid[arr[0]] !== ""){
+    winnerList.forEach((arr) => {
+        if (gameGrid[arr[0]] === gameGrid[arr[1]] && gameGrid[arr[1]] === gameGrid[arr[2]] && gameGrid[arr[0]] !== "") {
             boxes[arr[0]].classList.add('win');
             boxes[arr[1]].classList.add('win');
             boxes[arr[2]].classList.add('win');
             winner = gameGrid[arr[0]];
         }
     })
-    if(winner !== ""){
+    if (winner !== "") {
         disableCurser();
-        if(comp === true){
-            if(winner === 'X'){
+        if (comp === true) {
+            if (winner === 'X') {
                 winText.innerText = "Player Wins";
-            }else{
+            } else {
                 winText.innerText = "Computer Wins";
             }
-        }else{
+        } else {
             winText.innerText = `Winner - ${winner}`;
         }
         return true;
     }
 
     let count = 0;
-    gameGrid.forEach((x)=>{
-        if(x !== "")
+    gameGrid.forEach((x) => {
+        if (x !== "")
             count++;
     });
 
-    if(count === 9){
+    if (count === 9) {
         winText.innerText = `It's a Tie`;
         return true;
     }
@@ -91,39 +91,72 @@ function delay(duration) {
     return new Promise(resolve => setTimeout(resolve, duration));
 }
 
-async function handleClick(index){
-    if(gameGrid[index] === ""){
+function checkIfComputerCanWin(){
+    winnerList.forEach((arr)=>{
+        if (gameGrid[arr[0]] === gameGrid[arr[1]] && gameGrid[arr[1]] === '0' && gameGrid[arr[2]] === "") {
+            return arr[2];
+        }else if(gameGrid[arr[1]] === gameGrid[arr[2]] && gameGrid[arr[2]] === '0' && gameGrid[arr[0]] === ""){
+            return arr[0];
+        }else if(gameGrid[arr[2]] === gameGrid[arr[0]] && gameGrid[arr[2]] === '0' && gameGrid[arr[1]] === ""){
+            return arr[1];
+        }
+    });
+    return "";
+}
+
+function preventPlayerWinning(){
+    winnerList.forEach((arr)=>{
+        if (gameGrid[arr[0]] === gameGrid[arr[1]] && gameGrid[arr[1]] === 'X' && gameGrid[arr[2]] === "") {
+            return arr[2];
+        }else if(gameGrid[arr[1]] === gameGrid[arr[2]] && gameGrid[arr[2]] === 'X' && gameGrid[arr[0]] === ""){
+            return arr[0];
+        }else if(gameGrid[arr[2]] === gameGrid[arr[0]] && gameGrid[arr[2]] === 'X' && gameGrid[arr[1]] === ""){
+            return arr[1];
+        }
+    });
+    return -1;
+}
+
+async function handleClick(index) {
+    if (gameGrid[index] === "") {
         gameGrid[index] = player;
         boxes[index].innerText = player;
-        
+
         //Check if game complete
-        if(checkGameOver()){
+        if (checkGameOver()) {
             displayGameOver.classList.add('active');
             return;
         }
 
-        if(comp === true){
+        if (comp === true) {
             gameInfo.innerText = "Current Player - Computer";
-            let arr=[];
-            gameGrid.forEach((content, index)=>{
-                if(content === ""){
-                    arr.push(index);
-                }
-            });
-            let ind = Math.floor(Math.random() * arr.length);
+            let ind = checkIfComputerCanWin();
+            if (ind === -1) {
+                ind = preventPlayerWinning();
+            }
+            if (ind === -1) {
+                let arr = [];
+                gameGrid.forEach((content, index) => {
+                    if (content === "") {
+                        arr.push(index);
+                    }
+                });
+                let randInd = Math.floor(Math.random() * arr.length);
+                ind = arr[randInd];
+            }
             disableCurser();
             await delay(1000);
             enableCurser();
-            gameGrid[arr[ind]] = '0';
-            boxes[arr[ind]].innerText = '0';
-            if(checkGameOver()){
+            gameGrid[ind] = '0';
+            boxes[ind].innerText = '0';
+            if (checkGameOver()) {
                 displayGameOver.classList.add('active');
                 return;
             }
-        }else{
-            if(player === 'X'){
+        } else {
+            if (player === 'X') {
                 player = '0';
-            }else{
+            } else {
                 player = 'X';
             }
         }
@@ -131,18 +164,18 @@ async function handleClick(index){
     }
 }
 
-boxes.forEach((box, index)=>{
-    box.addEventListener('click', ()=>{
+boxes.forEach((box, index) => {
+    box.addEventListener('click', () => {
         handleClick(index);
     });
 })
 
-friend.addEventListener("click", ()=>{
+friend.addEventListener("click", () => {
     homeScreen.classList.remove("active");
     gameScreen.classList.add("active");
 });
 
-computer.addEventListener("click", ()=>{
+computer.addEventListener("click", () => {
     homeScreen.classList.remove("active");
     gameScreen.classList.add("active");
     comp = true;
